063b9a43fd3b1dcc469c43128fa6330d6babb32a
Dostan <turlybekdostan@gmail.com>
2025-06-29 20:45:37 +0500

    Initial Commit

---
+++ SwiftUIMasterCourse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIMasterCourse
+//
+//  Created by Dostan Turlybek on 29.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIMasterCourse/SwiftUIMasterCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMasterCourseApp.swift
+//  SwiftUIMasterCourse
+//
+//  Created by Dostan Turlybek on 29.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMasterCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
